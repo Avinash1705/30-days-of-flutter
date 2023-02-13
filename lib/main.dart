ed3a37000aa48da5d7cb240d87a64f4f6a7a07bc
Avinash696 <avinashrawat1705@gmail.com>
2023-02-13 17:19:13 +0530

    routes,theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_learn/home_page.dart';
+import 'package:flutter_learn/pages/home_page.dart';
+import 'package:flutter_learn/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/":(context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage(),
+      },
     );
   }
 }
