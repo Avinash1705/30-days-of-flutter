9bd6fe1d5d95866ea2e9e6b3d235774fddca9296
Avinash696 <avinashrawat1705@gmail.com>
2023-02-23 17:29:45 +0530

    data added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,55 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter_learn/utils/routes/MyRoutes.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_learn/widgets/item_widget.dart';
+import '../models/catelog.dart';
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loaFileDate();
+  }
+
+  void loaFileDate() async {
+    var jsonString = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeString = jsonDecode(jsonString)["products"]; // in object format
+    print(decodeString);
+    //mapping
+    CatelogModel.items = List.from(decodeString)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     var days = 30;
-    final String name = "Avinash";
+    const String name = "Avinash";
     return Scaffold(
       appBar: AppBar(
-        title: Text("Category App"),
+        title: const Text("Category App"),
       ),
       body: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to $days days of flutter by $name"),
-          ),
-        ),
+        child: (CatelogModel.items != null && CatelogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatelogModel.items.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    ItemWiget(item: CatelogModel.items[index]),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
